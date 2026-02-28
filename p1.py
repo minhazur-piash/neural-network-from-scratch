193daa506f2c56aa3f8ac783f4c69d88160d03d1
Minhazur Rahman <minhazur.piash@gmail.com>
2026-02-28 11:00:07 +0600

    add definition

--- p1.py
+++ p1.py
@@ -1,6 +1,18 @@
+import numpy as np
+
 inputs = [1, 2, 3.1]
 weights = [1.2, 2.1, 3.1]
 bias = 2
 
 output = sum([i * w for i, w in zip(inputs, weights)]) + bias
 print(output)
+
+inputs = [9]
+weights2 = [[1, 2, 3]]
+biases = [1, 2, 2]
+
+output = np.dot(inputs, weights) + biases
+print(output)
+
+print(np.random.rand(1, 3))
+print(np.random.rand(3,1))
